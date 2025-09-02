4430f2bc460c538ee604a81d0f0f0af8e9a9ca54
wahyusuryasaputra920 <wahyusuryasaputra920@gmail.com>
2025-09-02 05:18:38 +0000

    pertemuan ke-2

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Wahyu Surya Saputra'),
+      home: const MyHomePage(title: 'Wahyu Surya Saputra - 2307051025'),
     );
   }
 }
